776e42ed3603230e114a31823612daab37cf1671
havber <havber@gmail.com>
2015-12-02 09:32:32 +0100

    Upgraded to react 0.14.3. Added modal dialog for task creation.

--- src/js/components/views/App.jsx
+++ src/js/components/views/App.jsx
@@ -1,11 +1,26 @@
+import ReactDOM from 'react-dom';
 import React, {PropTypes} from 'react';
 import TaskList from './TaskList.jsx';
+import Modal from 'react-modal';
 
 export default React.createClass({
+
   propTypes: {
     tasks: PropTypes.array.isRequired,
     onAddTask: PropTypes.func.isRequired,
     onClear: PropTypes.func.isRequired
+  },
+
+  getInitialState: function() {
+    return {isModalOpen: false};
+  },
+
+  onOpenModal: function() {
+    this.setState({isModalOpen: true});
+  },
+
+  onCloseModal: function() {
+    this.setState({isModalOpen: false});
   },
 
   getDefaultProps() {
@@ -16,10 +31,23 @@ export default React.createClass({
 
   render() {
     let {onAddTask, onClear, tasks} = this.props;
+
     return (
       <div className="app-container">
         <TaskList tasks={tasks} />
-        <button onClick={onAddTask}>Add task</button>
+        <button onClick={this.onOpenModal}><span>Add task</span>
+
+          <Modal
+            isOpen={this.state.isModalOpen}
+            onRequestClose={this.onCloseModal} >
+
+            <button onClick={this.onCloseModal}>X</button>
+            <p>Add task:</p>
+            <input type="text"/>
+
+          </Modal>
+
+        </button>
       </div>
     );
   }
